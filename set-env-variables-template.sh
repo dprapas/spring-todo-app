a1766efdcb076187a172d593837f0f4223a3b746
dprapas <dimitrisprapas@gmail.com>
2021-11-17 13:48:52 +0200

    initial commit

---
+++ set-env-variables-template.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+export COSMOS_URI=https://cosmosdb-dprapas-todos.documents.azure.com:443/
+export COSMOS_KEY=ZeMJby6wy9HMpB4AsFHT3cSd7LZ3FIRcE0cCXM71bcT3vwdnIenyeq2zgxMoBMbfdmr7MIqlsm1vD9Ni1buwYA==
+export COSMOS_DATABASE=todosDB
+
+# App Service Linux Configuration
+export RESOURCEGROUP_NAME=cosmosdb-dprapas-rg
+export WEBAPP_NAME=todos-webapp
+export REGION=northeurope
